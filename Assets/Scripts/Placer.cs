b734afccaf5bd110fd29e00cf8886747d2947775
fogge1 <isak.fogge1@gmail.com>
2022-12-05 21:43:40 +0100

    add tiles

---
+++ Assets/Scripts/Placer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placer : MonoBehaviour
+{
+    public GameObject currentGameObject;
+    List<GameObject> map = new List<GameObject>();
+    public Transform selectedTransform;
+    private List<GameObject> _allowedMoves  = new List<GameObject>();
+    private GameObject[] _allowedToDestroy = new GameObject[]{};
+    // For future
+    private int indexOfPositions = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            Place();
+        }
+    }
+
+
+
+    void Place() {
+        _allowedToDestroy = GameObject.FindGameObjectsWithTag("allowedMoves");
+        foreach (GameObject allowedMove in _allowedToDestroy) {
+            Destroy(allowedMove);
+        }
+
+        GameObject addedTile = Instantiate(currentGameObject, selectedTransform.position, selectedTransform.rotation);
+
+        map.Add(addedTile);
+        // Need to empty allowedMoves array
+        _allowedMoves = new List<GameObject>();
+        for (int i = 0; i < addedTile.transform.childCount; i++) {
+            GameObject allowedMove = addedTile.transform.GetChild(i).gameObject;
+            _allowedMoves.Add(allowedMove);
+        }
+        indexOfPositions = 0;
+        selectedTransform = _allowedMoves[indexOfPositions].transform;
+
+
+    }
+}
