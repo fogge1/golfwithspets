a9bac7579c3603eaedb8786ce41461b668e424e7
abbmarand <markus.andersson@abbgymnasiet.se>
2022-12-06 16:13:21 +0100

    skapade branch där allt från min fork finns

---
+++ Assets/Markus/scripts/ballmovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmovement : MonoBehaviour
+{
+
+    public Rigidbody ball;
+
+    [SerializeField] public float shootpower = 10f;
+    [SerializeField] public float rotationspeed = 5f;
+    public LineRenderer line;
+
+    float yRot = 0f;
+    float xRot = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ball.position;
+        if (Input.GetMouseButton(0))
+        {
+            xRot += Input.GetAxis("Mouse X") * rotationspeed;
+            yRot += Input.GetAxis("Mouse Y") * rotationspeed;
+            if (yRot < -35f)
+            {
+                yRot = -35f;
+            }
+            transform.rotation = Quaternion.Euler(yRot, xRot, 0f);
+            line.gameObject.SetActive(true);
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, transform.position + transform.forward * 4f);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            ball.velocity = transform.forward * shootpower;
+            line.gameObject.SetActive(false);    
+        }
+    }
+   
+}
